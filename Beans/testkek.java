0c725332124079bc97431c3535768af8c0a8e619
runejo85 <runejo85@gmail.com>
2011-03-16 19:12:29 +0100

    daw

---
+++ Beans/testkek.java
@@ -0,0 +1,11 @@
+package java.Beans;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rune
+ * Date: 16.03.11
+ * Time: 19:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class testkek {
+}
